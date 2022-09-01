da9d2255937ba2a7c8f021870e50c8d2741be1c6
Harry <111663847+harrycodingnow@users.noreply.github.com>
2022-08-31 21:07:02 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+first_number = int(input("Enter Your first Number:"))
+math_operation = str(input("Enter your math symbol(+-*/):"))
+second_number = int(input("Enter Your second Number:"))
+if math_operation == ("+"):
+    print (first_number+second_number)
+if math_operation == ("-"):
+    print (first_number-second_number)
+if math_operation == ("*"):
+    print (first_number*second_number)
+if math_operation == ("/"):
+    print (first_number/second_number)
+
+
+
